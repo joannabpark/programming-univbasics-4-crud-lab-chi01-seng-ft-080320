2227f27e955ce16141e9c93f72e59333c2a47aea
JoAnna Park <joannabpark1@gmail.com>
2020-07-14 21:59:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,9 +8,8 @@ def
   ["one", "two", "three", "four"]
 end
 
-def add_element_to_end_of_array(array, element) 
-  add_element_to_end_of_array << element
-  
+def add_element_to_end_of_array(array, element)
+  << "hello"
 end
 
 def add_element_to_start_of_array(array, element)
